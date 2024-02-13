033e5a71c3f1f1bd543c7bbf7622cc520a358d58
Pedro S <p.sousa.silva88@gmail.com>
2024-02-13 21:55:38 +0000

    add result model

---
+++ src/RealEstateManagement.Application/Common/Models/Result.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace RealEstateManagement.Application.Common.Models;
+
+public class Result
+{
+    public static Result Success() => new(true, []);
+    public static Result Failure(IEnumerable<string> errors) => new(false, errors);
+
+    private Result(bool isSuccess, IEnumerable<string> errors) 
+    {
+        IsSuccess = isSuccess;
+        Errors = errors;
+    }
+
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public IEnumerable<string> Errors { get; }
+}
