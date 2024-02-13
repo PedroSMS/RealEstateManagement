0d6447cf91fc18abfc85c9efd824b00787324672
Pedro S <p.sousa.silva88@gmail.com>
2024-02-13 21:56:53 +0000

    update user controller

---
+++ RealEstateManagement.API/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using RealEstateManagement.Application.Authentication.Register;
+
+namespace RealEstateManagement.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpPost("register")]
+    public async Task<IResult> Register([FromServices]IMediator mediator, [FromBody]RegisterUserRequest request)
+    {
+        var result = await mediator.Send(request.ToCommand());
+
+        return result.IsSuccess
+            ? Results.NoContent() : Results.BadRequest(result.Errors);
+    }
+}
